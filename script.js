c6a2c931e2056867338ce7c26318a3182fdeebe2
dirmegle <dirmeityteegle@gmail.com>
2023-06-25 14:11:56 +0300

    nav bar

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+//Navbar
+
+
+
+// var tabs = document.getElementsByClassName("content")
+// var buttons = document.getElementsByTagName("button")
+
+// function changeTab(event, name){
+//     if(event.currentTarget.classList.contains("selected")) return
+
+//     // Nuimam visiems tabams block ir animate klases
+//     for(var tab of tabs){
+//         tab.classList.remove("block")
+//         tab.classList.remove("animate")
+//     }
+
+//     //Prideda block klase
+//     document.getElementById(name).classList.add("block")
+
+//     //Mygtuku stilius
+//     for(var button of buttons){
+//         button.classList.remove("selected")
+//     }
+
+//     event.currentTarget.classList.add("selected")
+
+//     // Animacija
+//     setTimeout(function(){
+//         document.getElementById(name).classList.add("animate")
+//     }, 100)
+// }
