597e7fd203b222c41c13fc54afa07b7f9e4fd315
dirmegle <dirmeityteegle@gmail.com>
2023-06-28 17:18:55 +0300

    navbar finalized

--- script.js
+++ script.js
@@ -1,41 +1,12 @@
 "use strict"
 
 //Navbar
-var burgerMenu = document.getElementById("burger-menu")
-var mobileNavbar = document.getElementById("mobile-navbar")
 
-burgerMenu.addEventListener('click', function() {
-    this.classList.toggle("open-menu");
-    mobileNavbar.classList.toggle("open")
-    // overlay.classList.toggle("overlay");
-  });
+var burger = document.getElementById("burger")
+var links = document.getElementsByClassName("links")[0]
 
+burger.addEventListener("click", function() {
+    links.classList.toggle("translate")
+    burger.classList.toggle("close")
+})
 
-
-// var tabs = document.getElementsByClassName("content")
-// var buttons = document.getElementsByTagName("button")
-
-// function changeTab(event, name){
-//     if(event.currentTarget.classList.contains("selected")) return
-
-//     // Nuimam visiems tabams block ir animate klases
-//     for(var tab of tabs){
-//         tab.classList.remove("block")
-//         tab.classList.remove("animate")
-//     }
-
-//     //Prideda block klase
-//     document.getElementById(name).classList.add("block")
-
-//     //Mygtuku stilius
-//     for(var button of buttons){
-//         button.classList.remove("selected")
-//     }
-
-//     event.currentTarget.classList.add("selected")
-
-//     // Animacija
-//     setTimeout(function(){
-//         document.getElementById(name).classList.add("animate")
-//     }, 100)
-// }
